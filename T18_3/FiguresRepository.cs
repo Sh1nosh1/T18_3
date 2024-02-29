54ae520f3aa496e987f470f801128bf8cfb9acfb
Shinda <123310732+Sh1nosh1@users.noreply.github.com>
2024-02-29 14:23:25 +0200

    Add files via upload

---
+++ T18_3/FiguresRepository.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace T18_3
+{
+    class FiguresRepository
+    {
+        /// <summary>
+        /// Colection of figures
+        /// </summary>
+        private List<Figure> figures = new List<Figure>();
+        public List<Figure> GetFigures()
+        {
+            return figures;
+        }
+        /// <summary>
+        /// Add figure
+        /// </summary>
+        /// <param name="figure">Figure</param>
+        public void CreateFigure(Figure figure)
+        {
+            figures.Add(figure);
+            WriteLine($"You have add a new figure - {figure.Name}\n");
+        }
+        /// <summary>
+        /// Remove shape
+        /// </summary>
+        /// <param name="figure">Shape</param>
+        public void RemoveFigure(string figure)
+        {
+            for (int i = figures.Count - 1; i >= 0; i--)
+            {
+                if (figures[i].Name == figure)
+                {
+                    figures.RemoveAt(i);
+                    WriteLine($"You have removed a figure - {figure}\n");
+                    return; // После удаления элемента нужно выйти из метода
+                }
+            }
+            WriteLine("Figure not found\n");
+        }
+        /// <summary>
+        /// Update 
+        /// </summary>
+        /// <param name="oldFigure">Old shape</param>
+        /// <param name="newFigure">New shape</param>
+        public void UpdateFigure(string oldFigure, Figure newFigure) 
+        {
+            for (int i = 0; i < figures.Count; i++)
+            {
+                if (figures[i].Name == oldFigure)
+                {
+                    figures[i] = newFigure;
+                    WriteLine($"You have edited {oldFigure} to {newFigure.Name}\n");
+                    return;
+                }
+            }
+            WriteLine("Figure not found\n");
+        }
+    }
+}
---
+++ T18_3/Triangle.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace T18_3
+{
+    class Triangle : Figure
+    {
+        /// <summary>
+        /// Auto fields
+        /// </summary>
+        public double Side1 {  get; set; }
+        public double Side2 { get; set; }
+        public double Side3 { get; set; }
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="name"></param>
+        /// <param name="side1"></param>
+        /// <param name="side2"></param>
+        /// <param name="side3"></param>
+        public Triangle(string name, double side1, double side2, double side3) : base(name)
+        {
+            Side1 = side1; Side2 = side2; Side3 = side3;
+        }
+        /// <summary>
+        /// Overriden method for calculating area of triangle
+        /// </summary>
+        /// <returns>Area</returns>
+        public override double Area()
+        {
+            double p = Perimeter() / 2;
+            return Math.Sqrt(p * (p - Side1) * (p - Side2) * (p - Side3));
+        }
+        /// <summary>
+        /// Overriden method for calculating perimeter of triangle
+        /// </summary>
+        /// <returns>PErimeter</returns>
+        public override double Perimeter()
+        {
+            return Side1 + Side2 + Side3;
+        }
+        /// <summary>
+        /// Output info
+        /// </summary>
+        public override string ToString()
+        {
+            return ($"Name: {Name}\nFirst side: {Side1}\nSecond side: {Side2}\nThird side: {Side3}\nArea: {Area()}\nPerimeter: {Perimeter()}\n");
+        }
+        /// <summary>
+        /// Input info
+        /// </summary>
+        /// <returns>new Triangle</returns>
+        public static Triangle Input()
+        {
+            Write("Enter name of figure: ");
+            string name = ReadLine();
+            Write("Enter first side: ");
+            double side1 = Convert.ToDouble(ReadLine());
+            Write("Enter second side: ");
+            double side2 = Convert.ToDouble(ReadLine());
+            Write("Enter third side: ");
+            double side3 = Convert.ToDouble(ReadLine());
+            return new Triangle(name, side1, side2, side3);
+        }
+    }
+}
